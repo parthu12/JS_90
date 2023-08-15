656ba0d1321651d02803d17938262745498cab26
Parthu Nallapu <parthunallapu@Parthus-MacBook-Pro.local>
2023-08-15 12:17:47 -0700

    HTML and JS and CSS

--- Day17/script.js
+++ Day17/script.js
@@ -1,10 +1,9 @@
-function scrolltop() {
-    let a = window.document;
-    a = location.href = '#home';
-}
-
-let contact= document.getElementById('contactForm');
-contact.addEventListner('submit', function(e){
+$("a[href='#scroll-top']").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+let contact = document.getElementById('contactForm');
+contact.addEventListener('submit', function (e) {
     e.preventDefault();
-    alert(`Sorry Request cannot be processed`);
+    alert(`Sorry Request cannot be accepted due to some technical errors`);
 })
